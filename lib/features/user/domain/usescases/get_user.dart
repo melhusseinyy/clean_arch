16036182d0a1d39579ef31b3164dd067a73d5f6d
Mohamed El Husseiny <mohamedellhusseiinyy9@gmail.com>
2025-08-11 12:19:39 +0300

    add user params

--- lib/features/user/domain/usescases/get_user.dart
+++ lib/features/user/domain/usescases/get_user.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch/core/errors/failure.dart';
+import 'package:clean_arch/core/params/params.dart';
 import 'package:clean_arch/features/user/domain/entities/user_entity.dart';
 import 'package:clean_arch/features/user/domain/repositories/user_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -7,7 +8,7 @@ class GetUser {
   final UserRepository repository;
 
   GetUser({required this.repository});
- Future<Either<Failure,UserEntity>> call(){
-    return repository.getUser();
+ Future<Either<Failure,UserEntity>> call({required UserParams params}){
+    return repository.getUser(params: params);
   }
 }
